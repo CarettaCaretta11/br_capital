fe07dc407e4bc52ee3a98cde766dc0fb2bd97e71
Kamran Mehdiyev <kamranmehdiyev@Kamrans-MacBook-Pro.local>
2022-12-18 01:11:36 +0900

    csrf-trusted-origins

--- br_capital/settings.py
+++ br_capital/settings.py
@@ -73,7 +73,7 @@ DATABASES = {
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
-        'PORT': '5432', 
+        'PORT': os.environ.get('DATABASE_PORT'),
     }
 }
 
